3939fefd80b0d423120fe6d756f696c8e1a60f2e
Nima Akbari <nima@qwil.io>
2021-02-12 14:09:31 +0000

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,6 +18,28 @@ function App() {
           Learn React
         </a>
       </header>
+      import React from 'react';
+import { HotTable } from '@handsontable/react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.data = [
+      ["", "Ford", "Volvo", "Toyota", "Honda"],
+      ["2016", 10, 11, 12, 13],
+      ["2017", 20, 11, 14, 13],
+      ["2018", 30, 15, 12, 13]
+    ];
+  }
+
+  render() {
+    return (
+      <div id="hot-app">
+        <HotTable data={this.data} colHeaders={true} rowHeaders={true} width="600" height="300" />
+      </div>
+    );
+  }
+}
     </div>
   );
 }
