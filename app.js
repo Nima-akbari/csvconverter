e37fb16f6d44b9e2970925591cb532986b4beb1f
Nima Akbari <nima@qwil.io>
2021-02-12 18:11:16 +0000

    test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var example = document.getElementById('example1');
 var hot1 = new Handsontable(example, {
-  data: Handsontable.helper.createSpreadsheetData(1000, 1000),
+  data: Handsontable.helper.createSpreadsheetData(100011, 100011),
   colWidths: 100,
   width: '100%',
   height: 320,
