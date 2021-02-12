bf79effa788497f47cc9005aa1ed0ea21d6ea410
Nima Akbari <nima@qwil.io>
2021-02-12 17:10:41 +0000

    test

---
+++ main.js
@@ -0,0 +1,25 @@
+var container1 = document.getElementById('example1');
+var hot1 = new Handsontable(container1, {
+  data: Handsontable.helper.createSpreadsheetData(7, 7),
+  colHeaders: true,
+  rowHeaders: true,
+  hiddenRows: { rows: [1, 3, 5], indicators: true },
+  hiddenColumns: { columns: [1, 3, 5], indicators: true }
+});
+var button1 = document.getElementById('export-file');
+var exportPlugin1 = hot1.getPlugin('exportFile');
+
+button1.addEventListener('click', function() {
+  exportPlugin1.downloadFile('csv', {
+    bom: false,
+    columnDelimiter: ',',
+    columnHeaders: false,
+    exportHiddenColumns: true,
+    exportHiddenRows: true,
+    fileExtension: 'csv',
+    filename: 'Handsontable-CSV-file_[YYYY]-[MM]-[DD]',
+    mimeType: 'text/csv',
+    rowDelimiter: '\r\n',
+    rowHeaders: true
+  });
+});
