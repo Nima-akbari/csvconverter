d4c8f5f724956d529621d5bf16d2c11c46585a8b
Nima Akbari <nima@qwil.io>
2021-02-12 19:33:40 +0000

    test

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
-var container1 = document.getElementById('example1');
-var hot1 = new Handsontable(container1, {
-  data: [],
-  dataSchema: {name: {first: null, last: null}, job_title: null, identifier: null, email: null, phoneNumber: null, groupidentifier: null, groupuuid: null},
-  startRows: 5,
-  startCols: 8,
-  colHeaders: ['First Name', 'Last Name', 'Job Title', 'Identifier', 'Email', 'Phone Number','Group Identifiers','Group UUIDS'],
-  columns: [
-    {data: 'name.first'},
-    {data: 'name.last'},
-    {data: 'job_title'},
-    {data: 'identifier'},
-    {data: 'email'},
-    {data: 'phoneNumber'},
-    {data: 'groupidenfier'},
-    {data: 'groupuuid'}
-  ],
-  minSpareRows: 1
-});
-var button1 = document.getElementById('export-file');
-var exportPlugin1 = hot1.getPlugin('exportFile');
+var people = [],
+example1 = document.getElementById('example1'),
+example1console = document.getElementById('example1console'),
+settings1,
+ipValidatorRegexp,
+emailValidator;
 
-button1.addEventListener('click', function() {
-  exportPlugin1.downloadFile('csv', {
-    bom: false,
-    columnDelimiter: ',',
-    columnHeaders: false,
-    exportHiddenColumns: true,
-    exportHiddenRows: true,
-    fileExtension: 'csv',
-    filename: 'Handsontable-CSV-file_[YYYY]-[MM]-[DD]',
-    mimeType: 'text/csv',
-    rowDelimiter: '\r\n',
-    rowHeaders: true
-  });
-});
+ipValidatorRegexp = /^(?:\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b|null)$/;
+emailValidator = function (value, callback) {
+setTimeout(function(){
+if (/.+@.+/.test(value)) {
+  callback(true);
+}
+else {
+  callback(false);
+}
+}, 1000);
+};
+
+settings1 = {
+data: people,
+beforeChange: function (changes, source) {
+for (var i = changes.length - 1; i >= 0; i--) {
+  // gently don't accept the word "foo" (remove the change at index i)
+  if (changes[i][3] === 'foo') {
+    changes.splice(i, 1);
+  }
+  // if any of pasted cells contains the word "nuke", reject the whole paste
+  else if (changes[i][3] === 'nuke') {
+    return false;
+  }
+  // capitalise first letter in column 1 and 2
+  else if ((changes[i][1] === 'name.first' || changes[i][1] === 'name.last')) {
+    if(changes[i][3] !== null){
+      changes[i][3] = changes[i][3].charAt(0).toUpperCase() + changes[i][3].slice(1);
+    }
+  }
+}
+},
+afterChange: function (changes, source) {
+if (source !== 'loadData') {
+  example1console.innerText = JSON.stringify(changes);
+}
+},
+rowHeaders: true,
+contextMenu: true,
+colHeaders: ['ID', 'First name', 'Last name', 'IP', 'E-mail'],
+columns: [
+{data: 'id', type: 'numeric'},
+{data: 'name.first'},
+{data: 'name.last'},
+{data: 'ip', validator: ipValidatorRegexp, allowInvalid: true},
+{data: 'email', validator: emailValidator, allowInvalid: false}
+]
+};
+var hot = new Handsontable(example1, settings1);
