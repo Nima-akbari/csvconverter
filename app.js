c845feb0e279fd1602b0225d0ef6ec417072911f
Nima Akbari <nima@qwil.io>
2021-02-12 15:11:03 +0000

    test

--- app.js
+++ app.js
@@ -1,30 +1,65 @@
-/* global FileReader, Papa, Handsontable */
+var
+  $$ = function(id) {
+    return document.getElementById(id);
+  },
+  container = $$('example1'),
+  exampleConsole = $$('example1console'),
+  autosave = $$('autosave'),
+  load = $$('load'),
+  save = $$('save'),
+  autosaveNotification,
+  hot;
 
-var input = document.getElementById('input-file')
-var handsontableContainer = document.getElementById('handsontable-container')
-
-input.onchange = function () {
-  var file = this.files[0]
-  var reader = new FileReader()
-
-  reader.onload = function (e) {
-    var csv = e.target.result
-    var data = Papa.parse(csv, {
-      header: true,
-      skipEmptyLines: true
-    })
-
-    // reset container
-    handsontableContainer.innerHTML = ''
-    handsontableContainer.className = ''
-
-    Handsontable(handsontableContainer, {
-      data: data.data,
-      rowHeaders: true,
-      colHeaders: data.meta.fields,
-      columnSorting: true
-    })
+hot = new Handsontable(container, {
+  startRows: 8,
+  startCols: 6,
+  rowHeaders: true,
+  colHeaders: true,
+  afterChange: function (change, source) {
+    if (source === 'loadData') {
+      return; //don't save this change
+    }
+    if (!autosave.checked) {
+      return;
+    }
+    clearTimeout(autosaveNotification);
+    ajax('scripts/json/save.json', 'GET', JSON.stringify({data: change}), function (data) {
+      exampleConsole.innerText  = 'Autosaved (' + change.length + ' ' + 'cell' + (change.length > 1 ? 's' : '') + ')';
+      autosaveNotification = setTimeout(function() {
+        exampleConsole.innerText ='Changes will be autosaved';
+      }, 1000);
+    });
   }
+});
 
-  reader.readAsText(file)
-}
+Handsontable.dom.addEvent(load, 'click', function() {
+  ajax('scripts/json/load.json', 'GET', '', function(res) {
+    var data = JSON.parse(res.response);
+
+    hot.loadData(data.data);
+    exampleConsole.innerText = 'Data loaded';
+  });
+});
+
+Handsontable.dom.addEvent(save, 'click', function() {
+  // save all cell's data
+  ajax('scripts/json/save.json', 'GET', JSON.stringify({data: hot.getData()}), function (res) {
+    var response = JSON.parse(res.response);
+
+    if (response.result === 'ok') {
+      exampleConsole.innerText = 'Data saved';
+    }
+    else {
+      exampleConsole.innerText = 'Save error';
+    }
+  });
+});
+
+Handsontable.dom.addEvent(autosave, 'click', function() {
+  if (autosave.checked) {
+    exampleConsole.innerText = 'Changes will be autosaved';
+  }
+  else {
+    exampleConsole.innerText ='Changes will not be autosaved';
+  }
+});
