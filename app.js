25c3dee341e3403f1ccf4e60b6db0825a8ad8c4f
Nima Akbari <nima@qwil.io>
2021-02-12 18:47:04 +0000

    test

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var container1 = document.getElementById('example1');
 var hot1 = new Handsontable(container1, {
   data: [],
-  
+  dataSchema: {name: {first: null, last: null}, job_title: null, identifier: null, email: null, phoneNumber: null, groupidentifier: null, groupuuid: null},
   startRows: 5,
   startCols: 8,
   colHeaders: ['First Name', 'Last Name', 'Job Title', 'Identifier', 'Email', 'Phone Number','Group Identifiers','Group UUIDS'],
-
+  columns: [
+    {data: 'id'},
+    {data: 'name.first'},
+    {data: 'name.last'},
+    {data: 'job_title'},
+    {data: 'identifier'},
+    {data: 'email'},
+    {data: 'phoneNumber'},
+    {data: 'groupidenfier'},
+    {data: 'groupuuid'}
+  ],
   minSpareRows: 1
 });
 var button1 = document.getElementById('export-file');
