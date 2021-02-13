a512283894b0e5707a6642f4c6f641e29edb9e4d
Nima Akbari <nima@qwil.io>
2021-02-13 00:00:18 +0000

    test

--- app.js
+++ app.js
@@ -82,3 +82,31 @@ button1.addEventListener('click', function() {
   });
 });
 
+var input = document.getElementById('input-file')
+var handsontableContainer = document.getElementById('handsontable-container')
+
+input.onchange = function () {
+  var file = this.files[0]
+  var reader = new FileReader()
+
+  reader.onload = function (e) {
+    var csv = e.target.result
+    var data = Papa.parse(csv, {
+      header: true,
+      skipEmptyLines: true
+    })
+
+    // reset container
+    handsontableContainer.innerHTML = ''
+    handsontableContainer.className = ''
+
+    Handsontable(handsontableContainer, {
+      data: data.data,
+      rowHeaders: true,
+      colHeaders: data.meta.fields,
+      columnSorting: true
+    })
+  }
+
+  reader.readAsText(file)
+}
